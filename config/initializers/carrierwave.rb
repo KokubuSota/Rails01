b4fdbb26f18c3e6cfefc65abde75462f1c4b0377
KOKUBU <kokkunbubu592@gmail.com>
2021-10-24 01:53:11 +0000

    1024-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'techboost20211016'
+  config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id:'AKIARJ2LVSA5HDTTXDH5',
-    aws_secret_access_key:'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',
-    region: 'ap-northeast-1',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
 end
